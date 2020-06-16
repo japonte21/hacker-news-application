a2c6b4da8d66737cc3d42d83978a66d773256ed7
Josiel M. Aponte <jma1@williams.edu>
2020-06-16 17:32:19 -0400

    Updates

---
+++ precache-manifest.066ea2ebd7d4b1f00f67c325dfee4ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5693a9b5b4df9dedd27af31687af1cc7",
+    "url": "/hacker-news-application/index.html"
+  },
+  {
+    "revision": "79c738a5c1d18ad406bb",
+    "url": "/hacker-news-application/static/css/main.6f101478.chunk.css"
+  },
+  {
+    "revision": "1e74e8dbc03f47acd3b0",
+    "url": "/hacker-news-application/static/js/2.edeec8bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news-application/static/js/2.edeec8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c738a5c1d18ad406bb",
+    "url": "/hacker-news-application/static/js/main.d7b80a5c.chunk.js"
+  },
+  {
+    "revision": "77ed3b75f1c1982753de",
+    "url": "/hacker-news-application/static/js/runtime-main.323cb634.js"
+  }
+]);
